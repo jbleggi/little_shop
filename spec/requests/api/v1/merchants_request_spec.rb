6df46a658fb946d049cd14510fe8c4b3b4e6047a
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-01-08 17:45:30 -0500

    Update Merchant Request rspec, 1/1 passing test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,14 +1,22 @@
 # bundle exec rspec spec/requests/api/v1/merchants_request_spec.rb
+
 require 'rails_helper'
 
-describe "Merchants API" do
+RSpec.describe "Merchants endpoints", type: :request do
   it "sends a list of merchants" do
     Merchant.create!(name: "Mike's Awesome Store")
     Merchant.create!(name: "Store of Fate")
     Merchant.create!(name: "This is the limit of my creativity")
 
-    get '/api/v1/merchants'
-
+    get "/api/v1/merchants"
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(merchants.count).to eq(3)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(Integer)
+    end
   end
 end
