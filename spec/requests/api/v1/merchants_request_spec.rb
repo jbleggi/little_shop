7c75173f0c6dfbb346e23d5c073b7ed8b0a60df8
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-01-09 14:54:30 -0500

    Update RSpec, 1/1 passing test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,6 +1,9 @@
 # bundle exec rspec spec/requests/api/v1/merchants_request_spec.rb
 
 require 'rails_helper'
+require 'simplecov'
+SimpleCov.start 'rails'
+
 
 RSpec.describe "Merchants endpoints", type: :request do
   it "sends a list of merchants" do
@@ -8,15 +11,20 @@ RSpec.describe "Merchants endpoints", type: :request do
     Merchant.create!(name: "Store of Fate")
     Merchant.create!(name: "This is the limit of my creativity")
 
-    get "/api/v1/merchants"
+    get '/api/v1/merchants'
+
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body, symbolize_names: true)
-    expect(merchants.count).to eq(3)
+  
+    expect(merchants[:data].count).to eq(3)
 
-    merchants.each do |merchant|
+    merchants[:data].each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:id]).to be_a(String)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
 end
