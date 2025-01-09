649b4522fd949c2eb7019c76bcd02e4cbb5e2735
Jackson Bickler <jbickler4@gmail.com>
2025-01-09 15:07:55 -0700

    controllers_jackson

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class ItemsController < ApplicationController
+      def destroy
+        item = Item.find_by(id: params[:id])
+        if item
+          item.destroy
+          head :no_content  
+        else
+          render json: { message: "Item not found", errors: ["Item with id #{params[:id]} does not exist"] }, status: 404
+        end
+      end
+    end
+  end
+end
