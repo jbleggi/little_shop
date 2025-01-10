683c7297f822f129816ccf29eb56580883f736f2
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-01-09 23:06:09 -0500

    Update merchant request spec to test for getting merchant by id

--- spec/merchants_request_spec.rb
+++ spec/merchants_request_spec.rb
@@ -20,7 +20,25 @@ RSpec.describe "Merchants API", type: :request do
       expect(merchant[:attributes]).to have_key(:created_at)
       expect(merchant[:attributes]).to have_key(:updated_at)
     end
+  end
 
+  it "can get one merchant by its id" do
+    merchant_example = create(:merchant)
+    merchant_id = merchant_example.id
+
+    get "/api/v1/merchants/#{merchant_id}"
+
+    merchant_response = JSON.parse(response.body, symbolize_names: true)
+    merchant_example = merchant_response[:merchant][:data][:attributes]
+    
+    expect(response).to be_successful
+
+    expect(merchant_example).to have_key(:id)
+    expect(merchant_example).to have_key(:name)
+    expect(merchant_example).to have_key(:created_at)
+    expect(merchant_example).to have_key(:updated_at)
+    
+    expect(merchant_response).to be_a Hash
   end
 
 end
