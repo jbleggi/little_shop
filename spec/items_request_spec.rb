d0296f44e07da99bc2dd2728dbb1ef29a6625100
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-01-09 22:39:03 -0500

    Created items request spec. First test passes.

---
+++ spec/items_request_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "Items API", type: :request do
+  it "sends a list of items" do
+    item1 = create(:item)
+    item2 = create(:item)
+    item3 = create(:item)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items[:data].count).to eq(3)
+    
+    items[:data].each do |item|
+      expect(item[:attributes]).to have_key(:id)
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes]).to have_key(:merchant_id)
+    end
+
+  end
+
+end
