0d1b100950b6fa3d41a5b3d04916be026aa52635
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-01-12 11:17:11 -0500

    Add merchant model spec. 2/2 RSpec pass, 100% SimpleCov.

---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,9 @@
+# bundle exec rspec spec/models/merchant_spec.rb
+require 'rails_helper'
+
+RSpec.describe Merchant, type: :model do
+  describe 'relationships' do
+    it { should have_many :invoices }
+    it { should have_many :items }
+  end
+end
