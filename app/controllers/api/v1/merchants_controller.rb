370bd41b5a003c49f508bdb3949b05707c25be13
Jackson Bickler <jbickler4@gmail.com>
2025-01-14 09:42:33 -0700

    merchant tets

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,13 +1,12 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    if params[:sorted] === 'age'
+    if params[:sorted] == 'age'
       merchants = Merchant.all.order(created_at: :desc)
-    else 
+    else
       merchants = Merchant.all
     end
-    
     render json: MerchantSerializer.new(merchants)
-    #(merchants, is_collection: true) ??
+    
   end
 
   def show
@@ -24,4 +23,45 @@ class Api::V1::MerchantsController < ApplicationController
       render json: MerchantSerializer.new(merchant)
     end
   end
-end 
+
+  def create
+    merchant = Merchant.new(merchant_params)
+
+    if merchant.save
+      render json: MerchantSerializer.new(merchant), status: :created
+    else
+      render json: { error: 'Unable to create merchant', details: merchant.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    merchant = Merchant.find_by(id: params[:id])
+
+    if merchant
+      if merchant.update(merchant_params)
+        render json: MerchantSerializer.new(merchant)
+      else
+        render json: { error: 'Unable to update merchant', details: merchant.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: 'Merchant not found' }, status: :not_found
+    end
+  end
+
+  def destroy
+    merchant = Merchant.find_by(id: params[:id])
+
+    if merchant
+      merchant.destroy
+      head :no_content 
+    else
+      render json: { error: 'Merchant not found' }, status: :not_found
+    end
+  end
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
+end
